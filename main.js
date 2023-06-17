33dfd4afdc8e2de01c600441a3073d920b4835fd
caffwydev <imchocoxd@mailfence.com>
2023-06-16 21:15:07 -0300

    CDN Done

--- main.js
+++ main.js
@@ -21,45 +21,51 @@ const repo = 'cdn';
 fetch(`https://api.github.com/repos/${owner}/${repo}/contents?nocache=${new Date().getTime()}`)
   .then(response => response.json())
   .then(data => {
+    console.log('Data retrieved, creating main UL')
     const container = document.getElementById('cdn');
 
     const ul = document.createElement('ul');
-    ul.type = "none"
+    ul.type = "none";
     container.appendChild(ul);
 
+    console.log('Adding data')
     data.forEach(item => {
       const li = document.createElement('li');
       ul.appendChild(li);
 
       if (item.type === 'file') {
+        console.log('Adding file ' + item.name)
+        if (item.name === ".create") return;
         const a = document.createElement('a');
         a.href = item.download_url;
         a.textContent = item.name;
         li.appendChild(a);
       } else if (item.type === 'dir') {
+        console.log('Adding dir ' + item.name)
         const span = document.createElement('span');
         span.textContent = item.name;
         li.appendChild(span);
 
         li.addEventListener('click', () => {
-            if (li.classList.contains('hiddenchild')) li.classList.remove('hiddenchild')
-            else li.classList.add('hiddenchild');
-        })
+          if (li.classList.contains('hiddenchild')) li.classList.remove('hiddenchild');
+          else li.classList.add('hiddenchild');
+        });
         fetch(item.url)
           .then(response => response.json())
           .then(data => {
             const subUl = document.createElement('ul');
-            subUl.type = "none"
+            subUl.type = "none";
             li.appendChild(subUl);
             data.forEach(subItem => {
               const subLi = document.createElement('li');
               subUl.appendChild(subLi);
-
               if (subItem.type === 'file') {
-                const a = document.createElement('a');
-                a.href = subItem.download_url;
-                a.textContent = subItem.name;
-                subLi.appendChild(a);
+                if (!(subItem.name === ".create")) {
+                  const a = document.createElement('a');
+                  a.href = subItem.download_url;
+                  a.textContent = subItem.name;
+                  subLi.appendChild(a);
+                }
               }
             });
           });
